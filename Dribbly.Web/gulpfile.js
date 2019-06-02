8cb7d0e286c1f0d8329a2bf1c371a3072b4aacba
RJ Pablo <numerianop@sedata.com>
2019-06-02 11:57:45 +0800

    setup: add gulpfile with initial tasks

---
+++ Dribbly.Web/gulpfile.js
@@ -0,0 +1,69 @@
+﻿/// <binding BeforeBuild='copy, build' />
+const gulp = require('gulp');
+const merge = require("merge-stream");
+const del = require("del");
+
+var paths = {
+    node: './node_modules/',
+    lib: './wwwroot/lib/'
+};
+
+var nodeLibs = {
+    jquery: {
+        source: 'jquery/dist/jquery.js',
+        destination: 'jquery'
+    },
+    angular: {
+        source: 'angular/angular.js',
+        destination: 'angular'
+    },
+    bootstrap: {
+        source: [
+            'bootstrap/dist/css/bootstrap.css',
+            'bootstrap/dist/js/bootstrap.bundle.js'
+        ],
+        destination: 'bootstrap'
+    }
+};
+
+// CLEAN //
+var dirsToClean = [
+    paths.lib
+];
+
+function clean() {
+    return del(dirsToClean);
+}
+
+gulp.task('clean', clean);
+
+// COPY //
+gulp.task('copy', copyNodeLibs);
+
+function copyNodeLibs() {
+    var streams = [];
+    var pckg = null;
+    Object.keys(nodeLibs).forEach(function (key) {
+        pckg = nodeLibs[key];
+        if (Array.isArray(pckg.source)) {
+            for (var i = 0; i < pckg.source.length; i++) {
+                pckg.source[i] = paths.node + pckg.source[i];
+            }
+        }
+        else {
+            pckg.source = paths.node + pckg.source;
+        }
+        streams.push(copy(pckg.source, paths.lib + pckg.destination));
+    });
+    return merge(streams);
+}
+
+function copy(source, destination) {
+    return gulp.src(source)
+        .pipe(gulp.dest(destination));
+}
+
+// BUILD //
+gulp.task('build', gulp.series('clean', 'copy'), function (done) {
+    done();
+});
