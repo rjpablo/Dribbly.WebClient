fd2661d6041fc5dc4a4306b74b1f582920ccf04a
RJ Pablo <numerianop@sedata.com>
2019-06-17 00:53:37 +0800

    feat: add temporary nav bar

---
+++ Dribbly.Web/wwwroot/shared/modules/site/components/navigator/navigator-controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('siteModule')
+        .component('drbblyNavigator', {
+            bindings: {},
+            controllerAs: 'nav',
+            templateUrl: '/shared/modules/site/components/navigator/navigator-template.html',
+            controller: controllerFn
+        });
+
+    controllerFn.$inject = [];
+    function controllerFn() {
+        var nav = this;
+        
+        nav.$onInit = function () {
+            nav.activeNavigationItem = 'courts';
+        };
+
+        nav.setActiveNavigationItem = function () {
+
+        }
+    }
+})();
