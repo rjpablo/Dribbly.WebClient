a99b93aedeafa9e585a123cb49112916deb41021
RJ Pablo <numerianop@sedata.com>
2019-06-17 01:19:34 +0800

    feat: add i18nService

---
+++ Dribbly.Web/wwwroot/shared/services/i18n-service.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('siteModule')
+        .service('i18nService', [function () {
+            var _entries = {
+                site: {
+                    Courts: 'Courts',
+                    Login: 'Login',
+                    Players: 'Players',
+                    SiteName: 'Dribbly',
+                    Teams: 'Teams'
+                }
+            };
+
+            function _getValue(key) {
+                var keys = key.split('.');
+                var app = _entries[keys[0]];
+                var value = app[keys[1]];
+                return value;
+            }
+
+            this.getValue = _getValue;
+
+            return this;
+        }]);
+})();
