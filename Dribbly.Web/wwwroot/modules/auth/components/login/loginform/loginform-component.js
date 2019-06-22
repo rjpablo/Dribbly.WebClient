e38d68caccb24dea5254ab984035280d4fff5b9f
RJ Pablo <numerianop@sedata.com>
2019-06-22 09:41:48 +0800

    feat: add login form component base

---
+++ Dribbly.Web/wwwroot/modules/auth/components/login/loginform/loginform-component.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('authModule')
+        .component('drbblyLoginform', {
+            bindings: {},
+            controllerAs: 'dhc',
+            templateUrl: '/modules/auth/components/login/loginform/loginform-template.html',
+            controller: controllerFunc
+        });
+
+    controllerFunc.$inject = [];
+    function controllerFunc() {
+        var dhc = this;
+    }
+})();
