847debbc465f17e563bea1d55f5bd94f2883198e
RJ Pablo <numerianop@sedata.com>
2019-06-22 11:01:28 +0800

    feat: convert site logo into a component

---
+++ Dribbly.Web/wwwroot/shared/modules/site/components/sitelogo/sitelogo-controller.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('siteModule')
+        .component('drbblySitelogo', {
+            bindings: {},
+            controllerAs: 'dsl',
+            templateUrl: '/shared/modules/site/components/sitelogo/sitelogo-template.html',
+            controller: controllerFn
+        });
+
+    controllerFn.$inject = [];
+    function controllerFn() {
+        var dsl = this;
+    }
+})();
