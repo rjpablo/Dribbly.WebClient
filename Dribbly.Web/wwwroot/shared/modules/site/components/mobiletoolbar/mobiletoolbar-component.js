ff2e9df11346d0294f4d98196c77efc52eaa4993
RJ Pablo <numerianop@sedata.com>
2019-07-11 02:15:53 +0800

    feat: add mobile toolbar

---
+++ Dribbly.Web/wwwroot/shared/modules/site/components/mobiletoolbar/mobiletoolbar-component.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('siteModule')
+        .component('drbblyMobiletoolbar', {
+            bindings: {},
+            controllerAs: 'mtb',
+            templateUrl: '/shared/modules/site/components/mobiletoolbar/mobiletoolbar-template.html',
+            controller: controllerFn
+        });
+
+    controllerFn.$inject = ['$element'];
+    function controllerFn($element) {
+        var mtb = this;
+        
+        mtb.$onInit = function () {
+            $element.addClass('mobile-toolbar');
+        };
+    }
+})();
