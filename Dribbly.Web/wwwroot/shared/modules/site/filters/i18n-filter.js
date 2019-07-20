24d82231a4c3fcaccba36cafbff55a88123ea645
RJ Pablo <numerianop@sedata.com>
2019-07-21 01:05:12 +0800

    feat: enable value substitutions for i18n keys

--- Dribbly.Web/wwwroot/shared/modules/site/filters/i18n-filter.js
+++ Dribbly.Web/wwwroot/shared/modules/site/filters/i18n-filter.js
@@ -4,8 +4,8 @@
     angular.module('siteModule')
         .filter('i18n', ['i18nService',
             function (i18nService) {
-                var i18nFilter = function (key) {
-                    return i18nService.getValue(key);
+                var i18nFilter = function (input, subs, options) {
+                    return i18nService.getString(input, subs, options);
                 };
 
                 return i18nFilter;
--- Dribbly.Web/wwwroot/shared/modules/site/services/i18n-service.js
+++ Dribbly.Web/wwwroot/shared/modules/site/services/i18n-service.js
@@ -9,7 +9,7 @@
                     Courts: 'Courts',
                     Email: 'Email',
                     Home: 'Home',
-                    LoggedInAs: 'logged in as ',
+                    LoggedInAs: 'logged in as <strong class="text-capitalize">{userName}</strong>',
                     LogOut: 'Log Out',
                     Or: 'Or',
                     Password: 'Password',
@@ -29,14 +29,124 @@
                 }
             };
 
-            function _getValue(key) {
-                var keys = key.split('.');
-                var app = _entries[keys[0]];
-                var value = app[keys[1]];
-                return value;
+            var thrownErrors = [];
+
+            function applySubstitutions(text, subs) {
+                var result = text;
+
+                if (subs) {
+                    angular.forEach(subs, function (value, key) {
+                        var subValue;
+                        if (value === null || value === undefined) {
+                            throwErrorIfNeeded('Value for ' + key + ' is not provided.');
+                            subValue = '';
+                        }
+                        else {
+                            subValue = value;
+                        }
+
+                        result = result.replace(new RegExp('{' + key + '}', 'g'), subValue);
+                    });
+                }
+                result = result.replace(/\n/g, '<br>');
+
+                return result;
+            }
+
+            function throwErrorIfNeeded(msg) {
+                //1. We don't want to interrupt the return of the value... 
+                //   If the service is being called by filters or directives.
+                //   That could break bindings, etc.
+                //2. We do want to tap into or error handling infrastructure.
+                //3. So we need to log, async.
+                //4. But we must do so within angular (vs. window.setTimeout()),
+                //   so that angular error handling can capture it.
+                //5. A single filter or directive binding call can happen many times,
+                //   during multiple angular digest cycles.  We don't want it to 
+                //   be logged a zillion times.  Once per page is enough.
+                //   So, we limit it here.
+                //6. $timeout will cause another digest cycle (which re-evaluates the
+                //   the filter, which can cause an endless loop if we were not to do #5.
+                //   invokeApply = false below should prevent this?  TODO: research.
+                //   But in the end it doesn't matter... we don't want to log multiples anyway.
+
+                if (!thrownErrors.includes(msg)) {
+                    thrownErrors.push(msg);
+                    $timeout(function () {
+                        throw new Error(msg);
+                    }, 1000, false);
+                }
+            }
+
+            function _getValue(token) {
+
+                var pieces = token.split('.');
+                if (pieces.length >= 2) {
+                    var library = _entries[pieces[0]];
+                    if (library) {
+                        var value = library[pieces[1]];
+                        //if enum...
+                        if (value && pieces.length === 3) {
+                            value = value[pieces[2]];
+                        }
+
+                        if (value) {
+                            return value;
+                        }
+                    }
+                }
+
+                return undefined;
+            }
+
+            function getLocalizedString(token, subs, options) {
+
+                if (token) {
+                    if (angular.isString(token)) {
+
+                        var value = _getValue(token);
+                        if (value) {
+                            return applySubstitutions(value, subs);
+                        }
+
+                        if (options && options.suppressErrors) {
+                            return token.split('.').atsLast(); //library.key --> key
+                        }
+
+                        var msg = '%%KEY_' + token + '_NOT_FOUND%%';
+                        //don't break function (log to the side)
+
+                        throwErrorIfNeeded(msg);
+
+                        return msg;
+                    }
+                    else if (angular.isArray(token)) {
+                        return getLocalizedString(token[0], token[1], options);
+                    }
+                    else if (angular.isObject(token)) {
+                        return getLocalizedString(token.token, token.subs, options);
+                    }
+                    else if (angular.isFunction(token)) {
+                        //handle selectors (i.e. dynamic tokens)
+                        return getLocalizedString(token(subs), subs, options);
+                    }
+                }
+
+                if (options && options.suppressErrors) {
+                    return getLocalizedString('site.Unknown'); //This will be displayed to users
+                }
+
+                /* eslint-disable no-redeclare */
+                var msg = '%%KEY_NOT_FOUND%%';
+                //don't break function (log to the side)
+
+                throwErrorIfNeeded(msg);
+
+                return msg;
             }
 
             this.getValue = _getValue;
+            this.getString = getLocalizedString;
 
             return this;
         }]);
