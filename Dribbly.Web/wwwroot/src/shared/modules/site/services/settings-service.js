9d386e6cb9781a2a2601f581235ca8d7ddb0b279
Rj Pablo <numerianop@sedata.com>
2020-08-26 08:55:56 +0800

    setup: ease setting of server to use

--- Dribbly.Web/wwwroot/src/shared/modules/site/services/settings-service.js
+++ Dribbly.Web/wwwroot/src/shared/modules/site/services/settings-service.js
@@ -8,10 +8,10 @@
         var _service = this;
         var _siteRoot = 'http://localhost:30585/';
         var _hostName = $location.host();
-        //var _serviceBase = 'https://localhost:44394/'; //use this when auth API is running in Visual Studio
-        //var _serviceBase = 'http://' + _hostName + ':1080/'; //use this to auth API in local IIS server
-        //var _serviceBase = 'http://localhost:1080/'; //use this to auth API in local IIS server
-        var _serviceBase = 'http://www.dribbly001.somee.com/'; //use somee test server
+        var _servicePreset = 2; // 1 => VS, 2 => online test server, 3 => local IIS
+        var _serviceBase = _servicePreset === 1 ? 'https://localhost:44394/' :
+            _servicePreset === 2 ? 'http://www.dribbly001.somee.com/' :
+                'http://' + _hostName + ':1080/';
         var _clientId = 'dribbly-web';
         var _clientSecret = '5YV7M1r981yoGhELyB84aC+KiYksxZf1OY3++C1CtRM=';
         var _settingsApiBaseUrl = 'api/settings/';
