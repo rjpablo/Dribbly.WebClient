0aa4817bc7160ed1b13d7ca29f755753c12a880f
Rj Pablo <numerianop@sedata.com>
2020-09-20 06:34:40 +0800

    feat: WEB-0136 - go to game details when notification is clicked

--- Dribbly.Web/wwwroot/src/shared/modules/app/components/notificationitem/notificationitem.component.js
+++ Dribbly.Web/wwwroot/src/shared/modules/app/components/notificationitem/notificationitem.component.js
@@ -11,12 +11,20 @@
             controller: controllerFn
         });
 
-    controllerFn.$inject = [];
-    function controllerFn() {
+    controllerFn.$inject = ['$state', 'constants'];
+    function controllerFn($state, constants) {
         var dni = this;
+        var _notificationTypes;
 
         dni.$onInit = function () {
-
+            _notificationTypes = constants.enums.notificationType;
+            setTargetLink();
         };
+
+        function setTargetLink() {
+            if (dni.item.type === _notificationTypes.gameBookedForBooker || dni.item.type === _notificationTypes.gameBookedForOwner) {
+                dni.targetLink = $state.href('main.game', { id: dni.item.gameId });
+            }
+        }
     }
 })();
