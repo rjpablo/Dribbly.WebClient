1416ca94b4d4895e54d642b067855b93b710a233
rjpablo <rjpablo.secondary@gmail.com>
2021-11-27 00:09:17 +0800

    setup: WEB0189 - Upgrade Web Client to AspNetCore 3.1

--- Dribbly.Web/Startup.cs
+++ Dribbly.Web/Startup.cs
@@ -37,7 +37,7 @@ namespace Dribbly.Web
             services.Configure<ClientSettings>(ClientSettingsSection);
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
