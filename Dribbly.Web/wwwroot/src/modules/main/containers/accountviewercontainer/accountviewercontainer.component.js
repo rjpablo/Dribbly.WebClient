cecad711c07a72e7a3a4e0caa9d72818b98129e6
rjpablo <rjpablo.secondary@gmail.com>
2023-08-12 21:10:28 +0800

    HOOP-166: fix issue: profile photo not going full screen when clicked

--- Dribbly.Web/wwwroot/src/modules/main/containers/accountviewercontainer/accountviewercontainer.component.js
+++ Dribbly.Web/wwwroot/src/modules/main/containers/accountviewercontainer/accountviewercontainer.component.js
@@ -12,8 +12,10 @@
             controller: controllerFunc
         });
 
-    controllerFunc.$inject = ['drbblyAccountsService', 'authService', '$stateParams', '$state', 'permissionsService'];
-    function controllerFunc(drbblyAccountsService, authService, $stateParams, $state, permissionsService) {
+    controllerFunc.$inject = ['drbblyAccountsService', 'authService', '$stateParams', '$state', 'permissionsService',
+        'modalService', 'drbblyFileService'];
+    function controllerFunc(drbblyAccountsService, authService, $stateParams, $state, permissionsService,
+        modalService, drbblyFileService) {
         var avc = this;
         var _username;
 
@@ -26,6 +28,7 @@
             drbblyAccountsService.getAccountViewerData(_username)
                 .then(function (data) {
                     avc.account = data.account;
+                    avc.account.profilePhoto = avc.account.profilePhoto || constants.images.defaultProfilePhoto;
                     avc.stats = data.stats;
                     avc.isOwned = authService.isCurrentUserId(avc.account.identityUserId);
                     avc.shouldDisplayAsPublic = avc.account.isPublic || avc.isOwned ||
@@ -43,6 +46,101 @@
 
         avc.$onDestroy = function () {
             avc.app.toolbar.clearNavItems();
+        };
+
+        avc.onProfilePhotoClick = function () {
+            modalService.showMenuModal({
+                model: {
+                    buttons: [
+                        {
+                            text: 'View Photo',
+                            action: viewPrimaryPhoto,
+                            class: 'btn-secondary'
+                        },
+                        {
+                            text: 'Replace Primary Photo',
+                            action: function () {
+                                angular.element('#btn-replace-photo').triggerHandler('click');
+                            },
+                            isHidden: () => !avc.isOwned && !permissionsService.hasPermission('Account.UpdatePhotoNotOwned'),
+                            class: 'btn-secondary'
+                        }
+                    ],
+                    hideHeader: true
+                },
+                size: 'sm',
+                backdrop: true
+            });
+        };
+
+        function viewPrimaryPhoto() {
+            drbblyAccountsService.getAccountPhotos(avc.account.id)
+                .then(function (photos) {
+                    avc.account.photos = massagePhotos(photos);
+                    var primaryPhotoIndex = avc.account.photos.findIndex(value => value.id === avc.account.profilePhotoId);
+                    avc.methods.open(primaryPhotoIndex);
+                })
+                .catch(function (error) {
+                    // TODO: display error in a toast
+                });
+        }
+
+        function massagePhotos(photos) {
+            var canDeleteNotOwned = permissionsService.hasPermission('Account.DeletePhotoNotOwned');
+            angular.forEach(photos, function (photo) {
+                photo.deletable = photo.id !== avc.account.profilePhotoId &&
+                    (avc.isOwned || canDeleteNotOwned);
+            });
+            return photos;
+        }
+
+        avc.onPrimaryPhotoSelect = function (file) {
+            if (!file) { return; }
+
+            var url = URL.createObjectURL(file);
+
+            return modalService.show({
+                view: '<drbbly-croppermodal></drbbly-croppermodal>',
+                model: {
+                    imageUrl: url,
+                    cropperOptions: {
+                        aspectRatio: 1
+                    }
+                }
+            })
+                .then(function (imageData) {
+                    var fileNameNoExt = (file.name.split('\\').pop().split('/').pop().split('.'))[0]
+                    imageData.name = fileNameNoExt + '.png';
+                    drbblyFileService.upload(imageData, 'api/account/uploadPrimaryPhoto/' + avc.account.id)
+                        .then(function (result) {
+                            if (result && result.data) {
+                                avc.account.profilePhoto = result.data;
+                                avc.account.profilePhotoId = result.data.id;
+                            }
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                })
+                .finally(function () {
+                    URL.revokeObjectURL(url)
+                });
+        };
+
+        avc.onDeletePhoto = function (img, callback) {
+            modalService.confirm('app.DeletePhotoConfirmationMsg')
+                .then(function (result) {
+                    if (result) {
+                        drbblyAccountsService.deletePhoto(img.id, avc.account.id)
+                            .then(function () {
+                                callback();
+                            })
+                            .catch(function (error) {
+                                // TODO: display error in a toast
+                                console.log('Error deleting photo', error);
+                            });
+                    }
+                });
         };
 
         function buildSubPages() {
