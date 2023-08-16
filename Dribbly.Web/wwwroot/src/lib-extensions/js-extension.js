f1a67acb626b70059b14b535111c17eb58e4ae9e
rjpablo <rjpablo.secondary@gmail.com>
2023-08-16 18:08:27 +0800

    HOOP-151: include libraries.js in bundle gulp task

--- Dribbly.Web/wwwroot/src/lib-extensions/js-extension.js
+++ Dribbly.Web/wwwroot/src/lib-extensions/js-extension.js
@@ -10,15 +10,15 @@
     };
 
     Number.prototype.toOrdinal = function () {
-        let ord = 'th';
+        var ord = 'th';
 
-        if (this % 10 == 1 && this % 100 != 11) {
+        if (this % 10 === 1 && this % 100 !== 11) {
             ord = 'st';
         }
-        else if (this % 10 == 2 && this % 100 != 12) {
+        else if (this % 10 === 2 && this % 100 !== 12) {
             ord = 'nd';
         }
-        else if (this % 10 == 3 && this % 100 != 13) {
+        else if (this % 10 === 3 && this % 100 !== 13) {
             ord = 'rd';
         }
 
