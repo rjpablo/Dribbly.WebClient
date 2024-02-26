5fe83717fcf7c77fdeec0608d78d2986654b5416
rjpablo <rjpablo.secondary@gmail.com>
2024-02-26 10:40:49 +0800

    HOOP-324: show indicator while loading profile photo of playerListItem

--- Dribbly.Web/wwwroot/src/modules/main/components/image/image.component.js
+++ Dribbly.Web/wwwroot/src/modules/main/components/image/image.component.js
@@ -9,6 +9,28 @@
                 onClick: '<'
             },
             controllerAs: 'dic',
-            templateUrl: 'drbbly-default'
+            templateUrl: 'drbbly-default',
+            controller: controllerFunc
         });
+
+    controllerFunc.$inject = ['$timeout', '$scope'];
+    function controllerFunc($timeout, $scope) {
+        var dic = this;
+
+        dic.$onInit = function () {
+            var image = new Image();
+            image.onload = function () {
+                $timeout(() => {
+                    dic.status = 'ready';
+                });
+            };
+            image.src = dic.imageUrl;
+            dic.status = 'loading';
+            image.onerror = function () {
+                $timeout(() => {
+                    dic.status = 'error';
+                });
+            }
+        };
+    }
 })();
